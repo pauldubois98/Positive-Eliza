978a8c684e83a0c52c0d46dff0ee7e93394f2d8e
pauldubois98 <pauldubois98@gmail.com>
2019-01-26 18:18:55 +0100

    start of eliza's "brain"

--- eliza.js
+++ eliza.js
@@ -1,7 +1,3 @@
-var conversation=document.getElementById("conversation");
-
-
-
 function write(text){
   var para = document.createElement("P");
   var t = document.createTextNode(text);
@@ -14,21 +10,57 @@ function userSay(text){
 function elizaSay(text){
   write("Eliza: "+text);
 }
+/*
+function makeElizaAnswer(query){
+  for(model in models){
+    if(query.search(model[0])){
+      console.log('found');
+    }
+  }
+
+}
+*/
 
 function userQuery(){
   if(document.getElementById("userInput").value===""){
 
   }else{
     userSay(document.getElementById("userInput").value);
+    makeElizaAnswer(document.getElementById("userInput").value);
     document.getElementById("userInput").value="";
-    makeElizaAnswer();
+  }
+}
+elizaSay("Hello, how are you today?");
+
+function makeElizaAnswer(query){
+  var regex;
+  var array;
+  for(model in models){
+    regex = RegExp(model[0], 'i');
+    array = regex.exec(query);
+
+    if( array!== null ){
+      console.log( array );
+    }
+    else {
+      console.log( "not found" );
+    }
   }
 
 }
 
-function makeElizaAnswer(){
-  
-}
+/*
+Typycal model:
+['espr', ["possible answer #1", "possible answer #2", ...]]
+Example:
+['(.*) if (.*)', ["A mathematical joke: is this an IFF statement?", \
+"Do you think it's likely that (2) => (1)?"]]
+*/
 
+models=[
+  ["(.*) if (.*)",
+    ["Right, but do you think (2) if (1)", "Do you think it's likely that (2)?"]],
+  ["(.*) else (.*)",
+    []],
 
-elizaSay("Hello, how are you today?")
+]
